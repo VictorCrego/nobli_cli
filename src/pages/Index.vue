8f7260d2ba5a6fccd87c325175a1c3c87fe6852a
Pedro Kohler <pedrokohlerbh@gmail.com>
2019-04-06 19:57:23 -0300

    Fix already initialized app bug.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -63,17 +63,20 @@ export default {
     }
   },
   mounted(){
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCSPzfPdAjl4kGNWYHEaAmjKnoBUiFuoh0",
-        authDomain: "appeventos-2ae82.firebaseapp.com",
-        databaseURL: "https://appeventos-2ae82.firebaseio.com",
-        projectId: "appeventos-2ae82",
-        storageBucket: "appeventos-2ae82.appspot.com",
-        messagingSenderId: "753074143663"
-    };
-    
-    firebase.initializeApp(config);
+
+    if(!firebase.apps.length){
+      // Initialize Firebase
+      var config = {
+          apiKey: "AIzaSyCSPzfPdAjl4kGNWYHEaAmjKnoBUiFuoh0",
+          authDomain: "appeventos-2ae82.firebaseapp.com",
+          databaseURL: "https://appeventos-2ae82.firebaseio.com",
+          projectId: "appeventos-2ae82",
+          storageBucket: "appeventos-2ae82.appspot.com",
+          messagingSenderId: "753074143663"
+      };
+      
+      firebase.initializeApp(config);
+    }
 
     firebase.auth().onAuthStateChanged(function(user){
       if(user == null){
